a97557c395826aee3aff7d86a90fe1ab7832a57d
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-08-12 09:50:42 +0200

    backup-servers.sh: also backup wiki (prints lot of error if needed.

--- bin/backup-servers.sh
+++ bin/backup-servers.sh
@@ -5,3 +5,12 @@
 # Backup giskard
 rsync -rl --delete giskard:$rem_dir_giskard_backup/ $dir_giskard_backup
 rsync -rl --delete giskard:$maildir_giskard $dir_giskard_backup
+
+## Dockuwiki (git)
+cd /home/jenselme/server/dokuwiki
+output=$(git pull)
+if [ $? -ne 0 ] ; then
+  date >> ~/error.log
+  pwd >> ~/error.log
+  echo $output >> ~/error.log
+fi
