68bdd165f676935e412b93e3244e27089dfd183b
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-09-27 19:32:11 +0200

    Maildir is backuped as any other backup.

--- bin/backup-servers.sh
+++ bin/backup-servers.sh
@@ -4,7 +4,6 @@
 
 # Backup giskard
 rsync -rl --delete giskard:$rem_dir_giskard_backup/ $dir_giskard_backup
-rsync -rl --delete giskard:$maildir_giskard $dir_giskard_backup
 
 ## Dockuwiki (git)
 cd /home/jenselme/server/dokuwiki
