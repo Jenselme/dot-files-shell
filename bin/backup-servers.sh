1f7f8181e7162635d6e5f8b5866bd3eaef8fdd35
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-10-02 10:01:54 +0200

    Remove the creation of error.log file.

--- bin/backup-servers.sh
+++ bin/backup-servers.sh
@@ -7,9 +7,4 @@ rsync -rl --delete giskard:$rem_dir_giskard_backup/ $dir_giskard_backup
 
 ## Dockuwiki (git)
 cd /home/jenselme/server/dokuwiki
-output=$(git pull)
-if [ $? -ne 0 ] ; then
-  date >> ~/error.log
-  pwd >> ~/error.log
-  echo $output >> ~/error.log
-fi
+git pull > /dev/null 2>&1
