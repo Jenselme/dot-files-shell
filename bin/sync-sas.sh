f0576ab1e0657ea1602427f03cc69effe8bb2cf0
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-11-13 21:55:17 +0100

    Sync private and html form sas.

--- bin/sync-sas.sh
+++ bin/sync-sas.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 DEST='/home/jenselme/Data/Archives/Scolarite/ECM/sas/'
-SOURCE1='jenselme:~/Documents/'
+SOURCE1='jenselme:~/Documents'
 SOURCE2='jenselme:~/workspace'
+SOURCE3='jenselme:~/private'
+SOURCE4='jenselme:~/html'
 
-rsync -rltpP --exclude *.mat --delete "$SOURCE1" "$DEST/Documents/"
-rsync -rltpP --delete "$SOURCE2" "$DEST"
+rsync -rltpP --exclude *.mat --delete "${SOURCE1}" "${DEST}"
+rsync -rltpP --delete "${SOURCE2}" "${DEST}"
+rsync -rltpP --delete --exclude clubdrupal "${SOURCE3}" "${DEST}"
+rsync -rltpP --delete "${SOURCE4}" "${DEST}"
