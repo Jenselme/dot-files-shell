6ab452493f66fc756a971776791a231b3c494392
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-01-13 15:08:56 +0100

    Correct build.sh and add an hepler function to exit on error with a message.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-! rpmbuild -bp "$1" && echo "rpmbuild -bp $1 && failed." >&2; exit 1;
-! rpmbuild -bc --short-circuit "$1" || echo "rpmbuild -bc --short-circuit $1 && failed" >&2; exit 2;
-! rpmbuild -bi --short-circuit "$1" || echo "rpmbuild -bi --short-circuit $1" >&2; exit 3;
-! rpmbuild -ba "$1" || echo "rpmbuild -ba $1 && failed." >&2; exit 4;
+source ~/.functions
+
+rpmbuild -bp "$1" || error_exit "rpmbuild -bp $1 && failed." 1
+rpmbuild -bc --short-circuit "$1" || error_exit "rpmbuild -bc --short-circuit $1 && failed" 2
+rpmbuild -bi --short-circuit "$1" || error_exit "rpmbuild -bi --short-circuit $1" 3
+rpmbuild -ba "$1" || error_exit "rpmbuild -ba $1 && failed." 4
