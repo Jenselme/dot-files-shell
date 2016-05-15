560e81c78b06da75afb4bc86dbece938ef419b8a
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-05-15 15:26:44 +0200

    Add script to build nbms for netbeans

---
+++ bin/nbbuildnbms.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ ! -d nbbuild ]]; then
+    echo "Launch this in nbbuild dir." >&2
+    exit 1
+fi
+
+unset JRE_HOME JAVA_BINDIR JAVA_HOME SDK_HOME JDK_HOME JAVA_ROOT
+export JAVA_HOME=/usr/java/jdk1.7.0_79
+export ANT_HOME=/usr/share/ant/
+export ANT_OPTS="-Xmx2048m -XX:MaxPermSize=384m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/antdump.txt -Dcluster.config=python"
+
+rm -rf nbbuild/nbms
+ant -Dcluster.config=python build-nonsigned-nbms
