6ea848dea707875914d28d0e46a16d4489519f82
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-09-13 21:34:48 +0200

    Apply debug patches before building nbms

--- bin/nbbuildnbms.sh
+++ bin/nbbuildnbms.sh
@@ -10,5 +10,15 @@ export JAVA_HOME=/usr/java/jdk1.7.0_79
 export ANT_HOME=/usr/share/ant/
 export ANT_OPTS="-Xmx2048m -XX:MaxPermSize=384m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/antdump.txt -Dcluster.config=python"
 
+pushd contrib
+    hg revent -a
+    for patchfile in ~/projects/nb-patches/jen-rel/*.patch; do
+        echo "Applying patch: ${patchfile}"
+        patch -p1 < "${patchfile}"
+    done
+popd
+
+
 rm -rf nbbuild/nbms
 ant -Dcluster.config=python build-nonsigned-nbms
+
