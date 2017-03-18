bee9170bc0f7cb80c408d844ac380dae17350261
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-03-18 17:52:06 +0100

    Update backup script
    - Make some paths specific to computer

--- bin/backup-data.sh
+++ bin/backup-data.sh
@@ -9,16 +9,10 @@ declare -A drsync_folders
 
 
 # drsync
-## VM
-drsync_folders["/run/media/jenselme/Data/VM/"]="/run/media/jenselme/Backup_2TB/VM"
-## Android
-drsync_folders["/run/media/jenselme/Data/android-sdk-linux/"]="/run/media/jenselme/Backup_2TB/android-sdk-linux"
 ## Projects data
 drsync_folders["/run/media/jenselme/Data/projects-data/"]="/run/media/jenselme/Backup_2TB/projects-data"
 ## Music
 drsync_folders["/home/jenselme/Music/"]="/run/media/jenselme/Backup_2TB/Music"
-## Archives
-drsync_folders["/run/media/jenselme/Data/Archives/"]="/run/media/jenselme/Backup_2TB/Archives"
 ## Downloads
 drsync_folders["/home/jenselme/Downloads/"]="/run/media/jenselme/Backup_2TB/Downloads"
 ## Books
@@ -27,10 +21,22 @@ drsync_folders["/home/jenselme/Livres/"]="/run/media/jenselme/Backup_2TB/Livres"
 drsync_folders["/home/jenselme/Projects/"]="/run/media/jenselme/Backup_2TB/projects"
 ## tests
 drsync_folders["/home/jenselme/tests/"]="/run/media/jenselme/Backup_2TB/tests"
-## Work
-drsync_folders["/home/jenselme/Work/"]="/run/media/jenselme/Backup_2TB/Work"
 ## Fedora
 drsync_folders["/home/jenselme/fedora-scm/"]="/run/media/jenselme/Backup_2TB/fedora-scm"
+
+# Compture specific
+case $(hostname -s) in
+    baley)
+        drsync_folders["/mnt/data/jenselme/VM/"]="/run/media/jenselme/Backup_2TB/VM_Baley"
+        drsync_folders["/mnt/data/jenselme/Jeux Videos/"]="/run/media/jenselme/Backup_2TB/Jeux Videos"
+        drsync_folders["/mnt/data/jenselme/Archives/"]="/run/media/jenselme/Backup_2TB/Archives"
+        ;;
+    fastolfe)
+        drsync_folders["/run/media/jenselme/Data/VM/"]="/run/media/jenselme/Backup_2TB/VM"
+        drsync_folders["/run/media/jenselme/Data/Archives/"]="/run/media/jenselme/Backup_2TB/Archives"
+        ;;
+esac
+
 
 for folder in "${!rsync_folders[@]}"; do
     echo -e "${folder}"
