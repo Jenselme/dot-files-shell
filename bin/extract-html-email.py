1ae7513ee68ea87b8662236d3c18a78e61d42901
Julien Enselme <jenselme@cpmultimedia.com>
2017-07-27 16:53:02 +0200

    bin: add script to extract HTML from emails

---
+++ bin/extract-html-email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+from glob import glob
+from mailbox import mboxMessage
+from os.path import join, splitext
+
+
+path = '.'
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+
+for path in glob(join(path, '*.mbox')):
+    with open(path, 'r') as mail_file:
+        message = mboxMessage(mail_file.read())
+
+    content = message.get_payload()[0]
+    html = content.get_payload(decode=True).decode('utf-8')
+    html = html.replace('charset=iso-8859-1', 'charset=utf-8')
+
+    file_name, _ = splitext(path)
+    with open(f'{file_name}.html', 'w') as html_file:
+        html_file.write(html)
+    
