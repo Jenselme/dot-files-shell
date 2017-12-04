7e3bc4a3eaa14fa00d58c26c2b8c744cdbe76cf5
Julien Enselme <jenselme@cpmultimedia.com>
2017-12-04 10:46:44 +0100

    bin: update async serve to latest version

--- bin/http-serve.py
+++ bin/http-serve.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python3
 
+import argparse
 import logging
 import mimetypes
 import os
+import re
 import aiofiles
 
+from glob import glob
 from stat import S_ISDIR
 from aiofiles import os as aios
 from aiohttp import web
+from aiohttp.web_urldispatcher import (
+    ResourceRoute,
+    UrlDispatcher,
+    UrlMappingMatchInfo,
+)
+
+
+class UrlToPathDispatcher(UrlDispatcher):
+    '''The gaol of this dispatcher is to load URLs like /images/world.png as path instead of routes.
+    '''
+    async def resolve(self, request):
+        return UrlMappingMatchInfo({}, ResourceRoute('GET', read_static, self))
 
 
 async def read_static(request):
-    text = "Hello World!"
-    path = request.query.get('path')
+    # Remove first slash to the page is correctly interpreted as a sub path of the current directory.
+    path = re.sub('^/', '', request.path)
+    # Replace empty path by current directory.
+    path = path if path else '.'
     if not await is_path_valid(path):
         logging.error(f'{path} is empty, does not exist or is not in the current directory')
         raise web.HTTPNotFound()
 
     if await is_directory(path):
-        raise web.HTTPNotImplemented()
+        return await serve_directory_or_index(path)
 
-    async with aiofiles.open(path, mode='rb') as f:
-        content = await f.read()
-        filename = os.path.basename(path)
-        content_type, charset = mimetypes.guess_type(filename)
-        return web.Response(
-            body=content,
-            content_type=content_type,
-            charset=charset,
-            headers={
-                'Content-Disposition': f'inline; filename="{filename}"'
-            },
-        )
+    return await serve_file(path)
 
 
 async def is_path_valid(path: str) -> bool:
@@ -60,8 +66,61 @@ async def is_directory(path: str) -> bool:
     return S_ISDIR(stat.st_mode)
 
 
-if __name__ == '__main__':
-    app = web.Application()
+async def serve_directory_or_index(path: str) -> web.Response:
+    if await has_index(path):
+        return await serve_file(os.path.join(path, 'index.html'))
+
+    return await serve_directory(path)
+
+
+async def has_index(path: str) -> bool:
+    return await exists(os.path.join(path, 'index.html'))
+
+
+async def serve_file(path: str) -> web.Response:
+    async with aiofiles.open(path, mode='rb') as f:
+        content = await f.read()
+        filename = os.path.basename(path)
+        content_type, charset = mimetypes.guess_type(filename)
+        return web.Response(
+            body=content,
+            content_type=content_type,
+            charset=charset,
+            headers={
+                'Content-Disposition': f'inline; filename="{filename}"'
+            },
+        )
+
+
+async def serve_directory(path: str) -> web.Response:
+    files = glob(os.path.join(path, '*'))
+    title = '/' if path == '.' else path
+    content = f'<h1>Listing under <em>{title}</em></h1> <ul>'
+
+    for file in files:
+        content += f'<li><a href="{file}">{file}</a></li>'
+
+    content += '</ul>'
+
+    return web.Response(text=content, content_type='text/html')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--port', '-p',
+        help='The port of which to launch the server',
+        default=8000,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    app = web.Application(router=UrlToPathDispatcher())
     app.router.add_get('/', read_static)
 
-    web.run_app(app, port=9292)
+    web.run_app(app, port=args.port)
+
+
+if __name__ == '__main__':
+    main()
+
