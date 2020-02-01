93911f52f84dc852f24d825a6d067403d9be98d7
Julien Enselme <jenselme@jujens.eu>
2020-02-01 17:01:08 +0100

    Update script to backup server

--- bin/backup-servers.sh
+++ bin/backup-servers.sh
@@ -3,6 +3,6 @@
 . /home/jenselme/bin/scripts-config.sh
 
 # Backup giskard
-rsync -a --delete "giskard:$rem_dir_giskard_backup/" "$dir_giskard_backup"
+rsync -a --delete --progress "giskard:$rem_dir_giskard_backup/" "$dir_giskard_backup"
 
 cd /home/jenselme/server/giskard-cfg && git pull > /dev/null 2>&1
