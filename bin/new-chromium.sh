398bb996472ee6f812ef949700c290fccc002d1e
Julien Enselme <julien.enselme@bankstore.fr>
2020-10-29 11:13:35 +0100

    Improve how we launch new Chromium instance

--- bin/new-chromium.sh
+++ bin/new-chromium.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 PROFILEDIR=$(mktemp -p /tmp -d tmp-fx-profile.XXXXXX.d)
-chromium-browser --user-data-dir=$PROFILEDIR --no-default-browser-check --no-first-run
+
+if [[ -f /usr/bin/chromium ]]; then
+    cmd=/usr/bin/chromium
+else
+    cmd=/usr/bin/chromium-browser
+fi
+
+"${cmd}" --user-data-dir=$PROFILEDIR --no-default-browser-check --no-first-run
 rm -rf $PROFILEDIR
