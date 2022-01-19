b8662e4f13c2fa9125e4b22cfbb76d103938b37b
Julien Enselme <jenselme@jujens.eu>
2022-01-19 19:14:41 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,11 @@ fish_add_path /usr/local/bin
 fish_add_path ~/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
-fish_add_path $HOME/.nvm/versions/node/v(cat ~/.nvm/alias/default)/bin
+fish_add_path ~/.nvm/versions/node/v(cat ~/.nvm/alias/default)/bin
 
 set -gx WORKON_HOME ~/.virtualenvs
-
 
 if status is-interactive
     starship init fish | source
     direnv hook fish | source
 end
-
