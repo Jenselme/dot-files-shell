54f6178a7352c87ab7dcf5fa589c03f2e701ef41
Julien Enselme <jenselme@jujens.eu>
2022-09-01 09:56:25 +0200

    Sync files with awscli directly instead of duplicity
    It doesn't work as expected with duplicity.

--- bin/backup-to-cold-storage.sh
+++ bin/backup-to-cold-storage.sh
@@ -10,52 +10,63 @@ function backup() {
     local extra_args="$@"
 
     for folder in "${!folders[@]}"; do
-        echo "Backuping ${folder} to ${SCW_BUCKET}/${folders[$folder]}"
+        echo "Backuping ${folder} to ${SCW_ARCHIVE_TARGET}/${folders[$folder]}"
+        sync-with-bucket "${SCW_ARCHIVE_TARGET}/${folders[$folder]}" "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
         duplicity incremental \
             --name=${folders[$folder]} \
             --full-if-older-than=1M \
-            --asynchronous-upload \
-            --s3-use-glacier \
             --progress \
             --encrypt-key=${DUPLICITY_GPG_ENCRYPT_KEY} \
             --sign-key=${DUPLICTIY_GPG_SIGN_KEY} \
             ${extra_args[@]} \
             "${folder}" \
-            "${SCW_BUCKET}/${folders[$folder]}"
+            "file://${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
+        sync-with-bucket "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}" "${SCW_ARCHIVE_TARGET}/${folders[$folder]}"
     done
+}
+
+function sync-with-bucket() {
+    local source="$1"
+    local dest="$2"
+
+    aws s3 sync "${source}" "${dest}" \
+            --profile duplicity \
+            --endpoint-url https://s3.fr-par.scw.cloud \
+            --delete
 }
 
 function verify() {
     for folder in "${!folders[@]}"; do
+        sync-with-bucket "${SCW_ARCHIVE_TARGET}/${folders[$folder]}" "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
         duplicity verify \
             --compare-data \
-            --s3-use-glacier \
             --encrypt-key=${DUPLICITY_GPG_ENCRYPT_KEY} \
             --sign-key=${DUPLICTIY_GPG_SIGN_KEY} \
-            "${SCW_BUCKET}/${folders[$folder]}" \
+            "file://${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}" \
             "${folder}"
     done
 }
 
 function list() {
     for folder in "${!folders[@]}"; do
-        echo "Listing files in ${SCW_BUCKET}/${folders[$folder]}"
+        echo "Listing files in ${SCW_ARCHIVE_TARGET}/${folders[$folder]}"
+        sync-with-bucket "${SCW_ARCHIVE_TARGET}/${folders[$folder]}" "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
         duplicity list-current-files \
-            --s3-use-glacier \
             --encrypt-key=${DUPLICITY_GPG_ENCRYPT_KEY} \
             --sign-key=${DUPLICTIY_GPG_SIGN_KEY} \
-            "${SCW_BUCKET}/${folders[$folder]}"
+            "file://${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
     done
 }
 
 function clean() {
     for folder in "${!folders[@]}"; do
         echo "Cleaning ${folders[$folder]}"
+        sync-with-bucket "${SCW_ARCHIVE_TARGET}/${folders[$folder]}" "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
         duplicity remove-older-than 6M \
-            --s3-use-glacier \
             --encrypt-key=${DUPLICITY_GPG_ENCRYPT_KEY} \
             --sign-key=${DUPLICTIY_GPG_SIGN_KEY} \
-            "${SCW_BUCKET}/${folders[$folder]}"
+            "file://${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
+        sync-with-bucket "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}" "${SCW_ARCHIVE_TARGET}/${folders[$folder]}"
     done
 }
 
@@ -64,6 +75,7 @@ function restore() {
     local restore_to=$(mktemp -d)
 
     echo "Restoring ${url_to_restore} to ${restore_to}"
+    sync-with-bucket "${SCW_ARCHIVE_TARGET}/${folders[$folder]}" "${DUPLICITY_ARCHIVE_TARGET}/${folders[$folder]}"
     duplicity restore \
         --s3-use-glacier \
         --encrypt-key=${DUPLICITY_GPG_ENCRYPT_KEY} \
@@ -77,15 +89,15 @@ function archive() {
     local dest="${archives[$path_to_save]}"
 
     echo "Saving ${path_to_save} to ${dest}"
+    sync-with-bucket "${SCW_ARCHIVE_TARGET}/${dest}" "${DUPLICITY_ARCHIVE_TARGET}/${dest}"
     duplicity full \
-        --s3-use-glacier \
-        --asynchronous-upload \
         --progress \
         --encrypt-key=${DUPLICITY_GPG_ENCRYPT_KEY} \
         --sign-key=${DUPLICTIY_GPG_SIGN_KEY} \
         "${path_to_save}" \
-        "${SCW_BUCKET}/${dest}"
-    duplicity remove-all-but-n-full 1 --force "${SCW_BUCKET}/${dest}"
+        "file://${DUPLICITY_ARCHIVE_TARGET}/${dest}"
+    duplicity remove-all-but-n-full 1 --force "file://${DUPLICITY_ARCHIVE_TARGET}/${dest}"
+    sync-with-bucket "${DUPLICITY_ARCHIVE_TARGET}/${dest}" "${SCW_ARCHIVE_TARGET}/${dest}"
 }
 
 if [[ $# -ne 1 ]]; then
