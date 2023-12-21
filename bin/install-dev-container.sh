e79bc56fb70fb1203e2cffccc878966d32be9081
Julien Enselme <jenselme@jujens.eu>
2023-12-22 00:05:07 +0100

    Create basic script to setup devcontainers

---
+++ bin/install-dev-container.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+
+sudo apt update
+sudo apt install -y zsh most curl wget direnv
+
+sudo curl -sS https://starship.rs/install.sh | sh -s -- --yes
+wget https://github.com/dandavison/delta/releases/download/0.16.5/delta-0.16.5-i686-unknown-linux-gnu.tar.gz
+tar -xvf delta-0.16.5-i686-unknown-linux-gnu.tar.gz
+sudo cp delta-0.16.5-i686-unknown-linux-gnu/delta /usr/local/bin
+rm -rf delta-0.16.5-i686-unknown-linux-gnu
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+git submodule update --init .oh-my-zsh/
+git submodule update --init .oh-my-zsh-custom/
+
+PROJECT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+cp -r $PROJECT_DIR/. ~/
+
+sudo chsh -s $(which zsh) $(whoami)
