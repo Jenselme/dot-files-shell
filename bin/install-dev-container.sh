e889a6346c0ec6ad213b41d690e308c38756b75d
Julien Enselme <jenselme@jujens.eu>
2023-12-24 15:25:29 +0100

    Install git by default

--- bin/install-dev-container.sh
+++ bin/install-dev-container.sh
@@ -3,7 +3,7 @@
 set -eu
 
 sudo apt update
-sudo apt install -y zsh most curl wget direnv
+sudo apt install -y zsh most curl wget direnv git
 
 sudo curl -sS https://starship.rs/install.sh | sh -s -- --yes
 wget https://github.com/dandavison/delta/releases/download/0.16.5/delta-0.16.5-i686-unknown-linux-gnu.tar.gz
